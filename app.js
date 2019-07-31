ce0dd849ae85efe8ad842bdef4949a83cdfe4c73
Omotayo Ikudayisi <omotayotitijames@gmail.com>
2019-07-31 01:08:38 +0100

    app.js
    Hamburger Icon achieved

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+      $('.sidenav').sidenav();
+      $('.materialboxed').materialbox();
+      $('.parallax').parallax();
+      $('.tabs').tabs();
+      $('.datepicker').datepicker({
+        disableWeekends: true,
+        yearRange: 1
+      });
+      $('.tooltipped').tooltip();
+      $('.scrollspy').scrollSpy();
+
+    });
